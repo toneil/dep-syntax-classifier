2b444ec3e0465ed2caab5df40694cbc7c090f592
Tobias Neil <brortobiasneil@gmail.com>
2016-11-28 16:16:46 +0100

    Classifier base module

---
+++ classifier.py
@@ -0,0 +1,20 @@
+from sklearn import linear_model
+
+def train_one_sample(feature_vector, cat, clf_wrapper, categories=None):
+    (clf, is_init) = clf_wrapper
+    if is_init:
+        clf.partial_fit([feature_vector], [cat])
+    else:
+        clf.partial_fit([feature_vector], [cat], categories)
+        return (clf, True)
+
+def make_classifier():
+    clf = linear_model.SGDClassifier()
+    return (clf, False)
+
+def write_to_file(classifier, path):
+    pass
+
+def predict_one(feature_vector, clf_wrapper):
+    (clf, _) = clf_wrapper
+    return clf.predict([feature_vector])
