3b702d182474f856613c8b7671600d8131000917
Tobias Neil <brortobiasneil@gmail.com>
2016-11-28 16:17:22 +0100

    Things work

--- features.py
+++ features.py
@@ -1,16 +1,19 @@
 import os
 import re
+import itertools
+import copy
+from nltk.stem import SnowballStemmer
 
-node_depth = 2
-arc_depth = 2
-ARC_SET = ['++','+A','+F','AA','AG','AN','AT','CA','DB','DT','EF','EO','ES','ET','FO','FP','FS','FV','I?','IC','IG','IK','IM','IO','IP','IQ','IR','IS','IT','IU','IV','JC','JG','JR','JT','KA','MA','MS','NA','OA','OO','OP','PL','PR','PT','RA','SP','SS','TA','TT','UK','VA','VO','VS','XA','XF','XT','XX','YY','CJ','HD','IF','PA','UA','VG']
+node_depth = 1
+arc_depth = 3
 
 # Reads conll file and returns a parse vector
 # p_v[doc] = (cat, [sents])
 # where sent = [(token, pos_tag, dep_head, label)]
 def read_parsed_sentences(file):
+    print("==> Reading parser output")
     parse_vector = []
-    line_pattern = re.compile(r'(\d+)\t(\S+)\t_\t(\w+)\t\w+\t(\w+)\t(\d+)\t(\w+)')
+    line_pattern = re.compile(r'(\d+)\t(\S+)\t_\t(\w+)\t\w+\t\S+\t(\d+)\t(\S+)')
     cat_pattern = re.compile(r'1\t_CAT_(\w+)')
     sentence_root = ('ROOT', '_', 0, '_')
     sentence = None
@@ -26,13 +29,13 @@ def read_parsed_sentences(file):
             continue
         match = line_pattern.match(line)
         if not match:
+            print("NO MATCH:", line)
             continue
         word_number = match.group(1)
         token = match.group(2)
         pos_tag = match.group(3)
-        morph = match.group(4)
-        dep_head = match.group(5)
-        label = match.group(6)
+        dep_head = match.group(4)
+        label = match.group(5)
         if word_number == '1':
             if sentence:
                 document[1].append(sentence)
@@ -42,10 +45,89 @@ def read_parsed_sentences(file):
     parse_vector.append(document)
     return parse_vector
 
-def make_feature_matrix(nodes, arcs, parse_vector):
-    pass
+def get_categories(parse_vector):
+    categories = set()
+    for (cat, _) in parse_vector:
+        categories.add(cat)
+    return categories
 
-with open('parseoutput.conll') as conll:
-    parse_v = read_parsed_sentences(conll)
-    feat_matrix = make_feature_matrix(node_depth, arc_depth, parse_v)
-    print (parse_v[2][1][4])
+def read_categories(cat_file):
+    categories = []
+    for cat in cat_file:
+        categories.append(cat)
+    return categories
+
+# Returns all unique tokens, pos_tags and arc labels in parse_vector
+def get_tokens(parse_vector):
+    stemmer = SnowballStemmer('swedish')
+    tokens = set(["__UNKNOWN_TOKEN__"])
+    for (_, sents) in parse_vector:
+        for sent in sents:
+            for (token, _, _, _) in sent:
+                tokens.add(stemmer.stem(token))
+    return tokens
+
+def make_feature_set(node_depth, arc_depth, parse_vector):
+    print("==> Gathering features")
+    feature_set = set(["__UNKNOWN_PATH__"])
+    no_of_docs = len(parse_vector)
+    docs_treated = 0
+    for (cat, sents) in parse_vector:
+        for sent in sents:
+            for (index, item) in enumerate(sent):
+                if index == 0:
+                    continue
+                try:
+                    arcs = tuple(map(lambda item: item[3], get_arcs_to_degree(sent, index, arc_depth)))
+                    nodes = tuple(map(lambda item: item[1], get_arcs_to_degree(sent, index, node_depth)))
+                # IndexErrors indicate upstream parsing failure
+                except:
+                    continue
+                combined = arcs + nodes
+                feature_set.add(combined)
+        docs_treated += 1
+        print("Treated {} documents of {}".format(docs_treated, no_of_docs))
+    return feature_set
+
+# Returns items following n arcs from start
+def get_arcs_to_degree(items, start, n):
+    returned_items = []
+    index = start
+    while n > 0:
+        #print(index, items)
+        next_item = items[index]
+        returned_items.append(next_item)
+        index = int(next_item[2])
+        n -= 1
+    return returned_items
+def make_empty_map(feature_list_file):
+    feature_map = dict()
+    for feature in feature_list_file:
+        feature = feature.strip()
+        feature_map[feature] = 0
+    return feature_map
+
+def make_feature_map(node_depth, arc_depth, doc, empty_feature_map, tokens):
+    feature_map = copy.copy(empty_feature_map)
+    for token in tokens:
+        if token not in feature_map:
+            feature_map['__UNKNOWN_TOKEN__'] += 1
+        else:
+            feature_map[token] += 1
+    (cat, sents) = doc
+    for sent in sents:
+        for (index, item) in enumerate(sent):
+            if index == 0:
+                continue
+            try:
+                arcs = tuple(map(lambda item: item[3], get_arcs_to_degree(sent, index, arc_depth)))
+                nodes = tuple(map(lambda item: item[1], get_arcs_to_degree(sent, index, node_depth)))
+            # IndexErrors indicate upstream parsing failure
+            except:
+                continue
+            combined = arcs + nodes
+            if combined not in feature_map:
+                feature_map['__UNKNOWN_PATH__'] += 1
+            else:
+                feature_map[combined] += 1
+    return feature_map
--- tag.py
+++ tag.py
@@ -4,10 +4,8 @@ import nltk
 from nltk.tag import HunposTagger
 from nltk import word_tokenize
 
-print('IMPORT DONE')
-
 def get_cat(docname):
-    pattern = r'\w+02([A-Za-z]+)\d+'
+    pattern = r'.*?\w+02([A-Za-z]+)\d+'
     m = re.match(pattern, docname)
     if m:
         return m.group(1)
@@ -28,33 +26,36 @@ def stripText(text):
         sub_text = pattern.sub("", sub_text)
     return sub_text
 
-def make_doclist(doc_dir_path):
+def make_doc_list(doc_dir_path):
     doclist = []
     doc_filenames = listdir(doc_dir_path)
     for doc_name in doc_filenames:
         doc_path = doc_dir_path + '/' + doc_name
-        with open(doc_path, 'r', encoding='utf-8') as doc_file:
-            cat = get_cat(doc_name)
-            if cat is None:
-                continue
-            text = stripText(doc_file.read())
-            doclist.append({
-                'cat': cat,
-                'text' : text
-            })
+        doc = make_doc(doc_path)
+        doclist.append(doc)
     return doclist
 
-def tokenize(doc_list):
+def make_doc(doc_path):
+    with open(doc_path, 'r', encoding='utf-8') as doc_file:
+        cat = get_cat(doc_path)
+        if cat is None:
+            cat = '__UNKNOWN_CAT__'
+        text = stripText(doc_file.read())
+        return {'cat': cat, 'text': text}
+
+def tokenize_list(doc_list):
     tokenized_list = []
     for doc in doc_list:
-        tokens = word_tokenize(doc['text'])
-        tokenized_list.append({
-            'tokens' : tokens,
-            'cat': doc['cat']
-        })
+        tokenized_doc = tokenize(doc)
+        tokenized_list.append(tokenized_doc)
     return tokenized_list
 
-def tag(tokenized_list, tagger):
+def tokenize(doc):
+    tokens = word_tokenize(doc['text'])
+    return {'cat': doc['cat'], 'tokens': tokens}
+
+def tag_list(tokenized_list, tagger):
+    print("==> Tagging doc list")
     pos_split_char = '_'
     tagged_list = []
     number_of_docs = len(tokenized_list)
@@ -62,29 +63,35 @@ def tag(tokenized_list, tagger):
     for doc in tokenized_list:
         i += 1
         print("{} of {} docs tagged".format(i, number_of_docs))
-        tagged = tagger.tag(doc['tokens'])
-        token_syntax_morph = []
-        for (token, stx_morph_bytes) in tagged:
-            if stx_morph_bytes is None:
-                continue
-            stx_morph = stx_morph_bytes.decode('utf-8')
-            syntax = stx_morph.split(pos_split_char)[0]
-            morph = pos_split_char.join(stx_morph.split(pos_split_char)[1:])
-            if token == '':
-                token = '_'
-            if morph == '':
-                morph = '_'
-            if syntax == '':
-                syntax = '_'
-            token_syntax_morph.append((token, syntax, morph))
-        tagged_list.append((doc['cat'], token_syntax_morph))
+        cat, token_syntax_morph = tag(doc, tagger)
+        tagged_list.append((cat, token_syntax_morph))
     return tagged_list
 
-def write_conll_file(path, tagged_list):
-    print('writing conll')
+def tag(tokenized_doc, tagger):
+    pos_split_char = '_'
+    tagged = tagger.tag(tokenized_doc['tokens'])
+    token_syntax_morph = []
+    for (token, stx_morph_bytes) in tagged:
+        if stx_morph_bytes is None:
+            continue
+        stx_morph = stx_morph_bytes.decode('utf-8')
+        syntax = stx_morph.split(pos_split_char)[0]
+        morph = pos_split_char.join(stx_morph.split(pos_split_char)[1:])
+        if token == '':
+            token = '_'
+        if morph == '':
+            morph = '_'
+        if syntax == '':
+            syntax = '_'
+        token_syntax_morph.append((token, syntax, morph))
+    return (tokenized_doc['cat'], token_syntax_morph)
 
+def write_conll_file(path, tagged):
+    print('==> Writing conll')
+    if type(tagged) != list:
+        tagged = [tagged]
     with open(path, 'w') as conll_file:
-        for (cat, token_syntax_morph) in tagged_list:
+        for (cat, token_syntax_morph) in tagged:
             print('\n1\t_CAT_{}\t_\t_\t_\t_\n'.format(cat), file=conll_file)
             row = 0
             for (token, syntax, morph) in token_syntax_morph:
@@ -94,19 +101,3 @@ def write_conll_file(path, tagged_list):
                 if morph == 'MAD':
                     print('', file=conll_file)
                     row = 0
-
-
-# Corpus file path
-DOC_DIR_PATH = 'testdata'
-# CoNLL file path
-CONLL_PATH = 'tagged.conll'
-# Tagger model path
-TAG_MODEL_PATH = 'model/suc-suctags.model'
-
-doc_list = make_doclist(DOC_DIR_PATH)
-tokenized_list = tokenize(doc_list)
-ht = HunposTagger(TAG_MODEL_PATH)
-tagged_list = tag(tokenized_list, ht)
-write_conll_file(CONLL_PATH, tagged_list)
-
-#java -jar maltparser-1.8.1/maltparser-1.8.1.jar -c swemalt-1.7.2.mco -m parse -i tagged.conll -o parseoutput.conll 
