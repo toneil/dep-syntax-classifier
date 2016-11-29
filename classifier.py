f146bef33c4afb7e441bc825f658a91d75dd2d11
Tobias Neil <brortobiasneil@gmail.com>
2016-11-29 12:17:31 +0100

    Save/load model

--- classifier.py
+++ classifier.py
@@ -1,4 +1,5 @@
 from sklearn import linear_model
+import pickle
 
 def train_one_sample(feature_vector, cat, clf_wrapper, categories=None):
     (clf, is_init) = clf_wrapper
@@ -13,8 +14,16 @@ def make_classifier():
     clf = linear_model.SGDClassifier()
     return (clf, False)
 
-def write_to_file(classifier, path):
-    pass
+def write_to_file(clf_wrapper, path):
+    (clf, _) = clf_wrapper
+    with open(path, 'w') as outfile:
+        clf.sparisfy()
+        pickle.dump(clf_wrapper, outfile)
+
+def load_from_file(path):
+    with open(path) as infile:
+        clf_wrapper = pickle.load(path)
+        return clf_wrapper
 
 def predict_one(feature_vector, clf_wrapper):
     (clf, _) = clf_wrapper
